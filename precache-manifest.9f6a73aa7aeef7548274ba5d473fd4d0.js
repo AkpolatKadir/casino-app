c142dd3580d70f7266814b46a40109a02b41c5f8
Kadir Akpolat <kadir.akpolat@eyedius.com>
2019-03-29 23:18:21 +0300

    Updates

---
+++ precache-manifest.9f6a73aa7aeef7548274ba5d473fd4d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9594a55cb70febb249f5",
+    "url": "/casino-app/static/js/runtime~main.6d664e72.js"
+  },
+  {
+    "revision": "fd6f71817c0907cfc079",
+    "url": "/casino-app/static/js/main.fc295c84.chunk.js"
+  },
+  {
+    "revision": "e06e59e280b6053641d6",
+    "url": "/casino-app/static/js/2.c1367858.chunk.js"
+  },
+  {
+    "revision": "fd6f71817c0907cfc079",
+    "url": "/casino-app/static/css/main.a18ac747.chunk.css"
+  },
+  {
+    "revision": "0af56b2141e798fd879ae8d37f1061cd",
+    "url": "/casino-app/index.html"
+  }
+];
